ce99f0202f127b09bf6b6e9de9eada6b9cd20f34
Kamleshgit1 <112052399+Kamleshgit1@users.noreply.github.com>
2022-08-25 09:50:43 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ x = int(input("Enter first number"))
 op = input("Enter the operator sign")
 y = int(input("Enter the second number"))
 if op == "+":
+    Print("Sum of this two number is : ")
     print(x+y)
 elif op == "-":
     print(x-y)
